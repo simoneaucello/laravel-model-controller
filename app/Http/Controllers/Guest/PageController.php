7be58f16dc1f023105795b98e354e1a76fd19abe
simoneaucello <simoneaucello@gmail.com>
2024-05-10 15:15:28 +0200

    [add] Model

--- app/Http/Controllers/Guest/PageController.php
+++ app/Http/Controllers/Guest/PageController.php
@@ -4,12 +4,14 @@ namespace App\Http\Controllers\Guest;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\Models\Movie;
 
 class PageController extends Controller
 {
     public function index()
     {
-        return view('home');
+        $movies = Movie::all();
+        return view('home', compact('movies'));
     }
 
     public function nuovaPagina()
