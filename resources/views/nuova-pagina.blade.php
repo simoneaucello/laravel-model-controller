f290f89b56e6938a1e6349129984400749fc73bb
Simone Aucello <152818802+simoneaucello@users.noreply.github.com>
2024-05-10 14:16:31 +0200

    Initial commit

---
+++ resources/views/nuova-pagina.blade.php
@@ -0,0 +1,5 @@
+@extends('layout.main')
+
+@section('content')
+    <h2>New Page</h2>
+@endsection
---
+++ resources/views/partials/header.blade.php
@@ -0,0 +1,6 @@
+<header>
+    <h1>HEADER</h1>
+
+    <a href="{{ route('home') }}">Home</a>
+    <a href="{{ route('nuova-pagina') }}">New Page</a>
+</header>
